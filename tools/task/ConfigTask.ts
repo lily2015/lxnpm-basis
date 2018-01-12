b1bc1d5f82c190637626a2aee0233bfa676deb8b
lixue <xue.li@mtime.com>
2018-01-12 15:21:52 +0800

    init

--- tools/task/ConfigTask.ts
+++ tools/task/ConfigTask.ts
@@ -1,25 +1,23 @@
 import * as gulp from "gulp";
 import * as jsonminify from "gulp-jsonminify";
-import * as path from "path";
 import { LoggerConsole as console } from "../libs/LoggerConsole";
 import { HelperTask } from "./HelperTask";
 
 export class ConfigTask {
-  private rootPath = path.resolve("./");
   public async run() {
     return new Promise((resolve, reject) => {
       const helperTask = new HelperTask();
       console.info("->", "ConfigTask", HelperTask.taking());
       gulp.task("ConfigTask", () => {
-        return gulp.src(this.rootPath + "config/**/*.json")
-          // .on("end", () => {
-          //   console.info("ConfigTask.done");
-          // }).on("error", (error: any) => {
-          //   console.error("ConfigTask.error", error.message);
-          //   helperTask.sendMessage("ConfigTask-Error", error.message);
-          // })
+        return gulp.src("config/**/*.json")
+          .on("end", () => {
+            console.info("ConfigTask.done");
+          }).on("error", (error: any) => {
+            console.error("ConfigTask.error", error.message);
+            helperTask.sendMessage("ConfigTask-Error", error.message);
+          })
           .pipe(jsonminify())
-          .pipe(gulp.dest(this.rootPath + "build/config"));
+          .pipe(gulp.dest("build/config"));
       });
       gulp.start("ConfigTask", (error: any) => {
         if (error) {
